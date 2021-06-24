2d8555d08e42b6986faebae6054aee580e0bfd88
ethan.xingyu.ji <xingyu.ji@accenture.com>
2021-06-24 14:17:05 +1000

    fix: add all args constructor for customer

--- src/main/java/com/xingyu/domain/Customer.java
+++ src/main/java/com/xingyu/domain/Customer.java
@@ -1,12 +1,16 @@
 package com.xingyu.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Column
     private String name;
