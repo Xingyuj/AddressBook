b1d78cb112da5c363a9dafdf8097b8c946555e3e
ethan.xingyu.ji <xingyu.ji@accenture.com>
2021-06-24 16:16:55 +1000

    feat: add dockerfile and start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo running docker container
+docker-compose up -d
+echo wait for db container to get ready
+sleep 10
+echo wait for db container to get ready
+sleep 10
+echo insert data into db
+docker cp ./dump.sql xingyu-postgres:/docker-entrypoint-initdb.d/dump.sql
+docker exec -u postgres xingyu-postgres psql postgres postgres -f docker-entrypoint-initdb.d/dump.sql
+echo done! address book is ready to play!
