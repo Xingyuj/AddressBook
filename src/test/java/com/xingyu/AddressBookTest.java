72ac136d58ae9936c835736741a8eb42f1a1d9e1
ethan.xingyu.ji <xingyu.ji@accenture.com>
2021-06-24 11:09:36 +1000

    feat: add unit tests

---
+++ src/test/java/com/xingyu/AddressBookTest.java
@@ -0,0 +1,30 @@
+package com.xingyu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddressBookTest {
+
+    @Test
+    public void testListAddressBook() {
+    }
+
+    @Test
+    public void testGetAddressBookById() {
+    }
+
+    @Test
+    public void testCreateAddressBook() {
+    }
+
+    @Test
+    public void testUpdateAddressBook() {
+    }
+
+    @Test
+    public void testDeleteAddressBook() {
+    }
+
+}
